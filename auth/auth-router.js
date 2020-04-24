bacd8545802f2b84842cee21c1bdb864455bdd39
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-24 09:46:58 -0600

    1st pass at register set up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const gt = require('./generateToken.js');
+const Users = require('./auth-model.js');
+
+
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const rounds = process.env.HASH_ROUNDS || 6;
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = gt.generateToken(user)
+      res.status(201).json({saved, token, message: "User Created"})
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
 });
 
 router.post('/login', (req, res) => {
