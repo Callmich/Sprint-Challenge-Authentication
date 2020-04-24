cc8053cc8b5a9f8cc2f9585f392654de6064c6d2
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-24 09:52:07 -0600

    1st pass at register written

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,7 +23,25 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = gt.generateToken(user)
+        res.status(200).json({
+          message: `welcome ${user.username}`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error);
+    })
 });
 
 module.exports = router;
